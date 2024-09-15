839f66cd20379583d3b5e93d60a00d705b42edc5
EliabParra <117409776+EliabParra@users.noreply.github.com>
2024-09-14 20:53:25 -0400

    a

--- classes/Email.php
+++ classes/Email.php
@@ -32,95 +32,82 @@ class Email {
     }
 
     public function enviarConfirmacion() {
-        //crear el obj de email
-        // $mail = new PHPMailer();
-        // $mail->isSMTP();
-        // $mail->Host = $_ENV['EMAIL_HOST'];
-        // $mail->SMTPAuth = true;
-        // $mail->Port = $_ENV['EMAIL_PORT'];
-        // $mail->Username = $_ENV['EMAIL_USER'];
-        // $mail->Password = $_ENV['EMAIL_PASS'];
+        // crear el obj de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = $_ENV['EMAIL_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Port = $_ENV['EMAIL_PORT'];
+        $mail->Username = $_ENV['EMAIL_USER'];
+        $mail->Password = $_ENV['EMAIL_PASS'];
 
-        // $mail->setFrom('cuentas@appsalon.com');
-        // $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
-        // $mail->Subject = 'Confirma tu Cuenta';
+        $mail->setFrom('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->Subject = 'Confirma tu Cuenta';
 
-        // //set HTML
-        // $mail->isHTML(TRUE);
-        // $mail->CharSet = 'UTF-8';
+        //set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
 
-        // $contenido = "
-        //     <html>
-        //         <head>
-        //             {$this->font}
-        //             {$this->styles}
-        //         </head>
-        //         <body>
-        //             <h1>Hola " . $this->nombre . "</h1>
-        //             <p>Has creado tu cuenta en AppSalon, solo debes confirmarla presionando el siguiente enlace</p>
-        //             <p><strong>Presiona aquí: </strong></p>
-        //             <a href='{$_ENV['APP_URL']}/confirmar-cuenta?token=". $this->token ."'>Confirmar Cuenta</a>
-        //             <p id='texto-mini'>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>
-        //         </body>
-        //     </html>
-        // ";
+        $contenido = "
+            <html>
+                <head>
+                    {$this->font}
+                    {$this->styles}
+                </head>
+                <body>
+                    <h1>Hola " . $this->nombre . "</h1>
+                    <p>Has creado tu cuenta en AppSalon, solo debes confirmarla presionando el siguiente enlace</p>
+                    <p><strong>Presiona aquí: </strong></p>
+                    <a href='{$_ENV['APP_URL']}/confirmar-cuenta?token=". $this->token ."'>Confirmar Cuenta</a>
+                    <p id='texto-mini'>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>
+                </body>
+            </html>
+        ";
 
-        // $mail->Body = $contenido;
+        $mail->Body = $contenido;
 
-        // //enviar el email
-        // $mail->send();
-
-        $resend = Resend::client('re_123456789');
-
-        try {
-            $result = $resend->emails->send([
-                'from' => 'Acme <onboarding@resend.dev>',
-                'to' => [$this->email],
-                'subject' => "Hola {$this->nombre}",
-                'html' => '<strong>It works!</strong>',
-            ]);
-        } catch (\Exception $e) {
-            exit('Error: ' . $e->getMessage());
-        }
+        //enviar el email
+        $mail->send();
     }
 
     public function enviarInstrucciones() {
-        //crear el obj de email
-        // $mail = new PHPMailer();
-        // $mail->isSMTP();
-        // $mail->Host = $_ENV['EMAIL_HOST'];
-        // $mail->SMTPAuth = true;
-        // $mail->Port = $_ENV['EMAIL_PORT'];
-        // $mail->Username = $_ENV['EMAIL_USER'];
-        // $mail->Password = $_ENV['EMAIL_PASS'];
+        // crear el obj de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = $_ENV['EMAIL_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Port = $_ENV['EMAIL_PORT'];
+        $mail->Username = $_ENV['EMAIL_USER'];
+        $mail->Password = $_ENV['EMAIL_PASS'];
 
-        // $mail->setFrom('cuentas@appsalon.com');
-        // $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
-        // $mail->Subject = 'Reestablece tu Password';
+        $mail->setFrom('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->Subject = 'Reestablece tu Password';
 
-        // //set HTML
-        // $mail->isHTML(TRUE);
-        // $mail->CharSet = 'UTF-8';
+        //set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
 
-        // $contenido = "
-        //     <html>
-        //         <head>
-        //             {$this->font}
-        //             {$this->styles}
-        //         </head>
-        //         <body>
-        //             <h1>Hola " . $this->nombre . "</h1>
-        //             <p>Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo</p>
-        //             <p><strong>Presiona aquí: </strong></p>
-        //             <a href='{$_ENV['APP_URL']}/reestablecer?token=". $this->token ."'>Reestablecer Password</a>
-        //             <p id='texto-mini'>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>
-        //         </body>
-        //     </html>
-        // ";
+        $contenido = "
+            <html>
+                <head>
+                    {$this->font}
+                    {$this->styles}
+                </head>
+                <body>
+                    <h1>Hola " . $this->nombre . "</h1>
+                    <p>Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo</p>
+                    <p><strong>Presiona aquí: </strong></p>
+                    <a href='{$_ENV['APP_URL']}/reestablecer?token=". $this->token ."'>Reestablecer Password</a>
+                    <p id='texto-mini'>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>
+                </body>
+            </html>
+        ";
 
-        // $mail->Body = $contenido;
+        $mail->Body = $contenido;
 
-        // //enviar el email
-        // $mail->send();
+        //enviar el email
+        $mail->send();
     }
 }
